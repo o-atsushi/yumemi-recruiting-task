6177853fead3fa4d78502606fb93bd149e65f763
okadaatsushi <atsushi.okada.09211@gmail.com>
2020-01-24 19:04:33 +0900

    :up:

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,6 @@
 const Constants = {
-  resas_base_url: "https://opendata.resas-portal.go.jp";
+  resas_base_url: "https://opendata.resas-portal.go.jp",
+  resas_api_key: "ri2X4L3V8zCadFpmxNjN58ppxDiAOebDanYF7LZZ",
 }
 
 export default Constants;
